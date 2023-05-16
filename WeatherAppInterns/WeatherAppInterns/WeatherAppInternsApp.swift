cda878c3dfbc0b45f62aea446a5c415bf2eb8646
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-05-16 07:43:40 -0700

    Initial Commit
    Created the initial commit with the WeatherAppInterns template for SwiftUI

---
+++ WeatherAppInterns/WeatherAppInterns/WeatherAppInternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppInternsApp.swift
+//  WeatherAppInterns
+//
+//  Created by Moises Lopez on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppInternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
