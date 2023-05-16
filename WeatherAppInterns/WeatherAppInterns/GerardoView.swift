70a2d76e2d9d6cf30a49cb0faea8940ae7dea6ed
Gerardo Ramos <gerx_37@hotmail.com>
2023-05-16 10:14:05 -0600

    add gerardoview
    created gerardoview doc

---
+++ WeatherAppInterns/WeatherAppInterns/GerardoView.swift
@@ -0,0 +1,20 @@
+//
+//  GerardoView.swift
+//  WeatherAppInterns
+//
+//  Created by Gerardo Leal on 16/05/23.
+//
+
+import SwiftUI
+
+struct GerardoView: View {
+    var body: some View {
+        Text("Prueba")
+    }
+}
+
+struct GerardoView_Previews: PreviewProvider {
+    static var previews: some View {
+        GerardoView()
+    }
+}
