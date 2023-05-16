9e6b707e6896cf78bf8408ca4adf945281f52202
Angel Avalos <leonavalos4772@gmail.com>
2023-05-16 10:35:25 -0600

    MyView
    This is a test

---
+++ WeatherAppInterns/WeatherAppInterns/AngelView.swift
@@ -0,0 +1,20 @@
+//
+//  AngelView.swift
+//  WeatherAppInterns
+//
+//  Created by Leo Avalos on 16/05/23.
+//
+
+import SwiftUI
+
+struct AngelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AngelView_Previews: PreviewProvider {
+    static var previews: some View {
+        AngelView()
+    }
+}
