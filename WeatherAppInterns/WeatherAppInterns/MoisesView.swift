d270c0619df0a0662c7397a39c7c1b693391082b
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-05-16 09:18:41 -0700

    Added MoisesView
    I add moisesView to the branch

---
+++ WeatherAppInterns/WeatherAppInterns/MoisesView.swift
@@ -0,0 +1,20 @@
+//
+//  MoisesView.swift
+//  WeatherAppInterns
+//
+//  Created by Moises Lopez on 16/05/23.
+//
+
+import SwiftUI
+
+struct MoisesView: View {
+    var body: some View {
+        Text("MoisesView")
+    }
+}
+
+struct MoisesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoisesView()
+    }
+}
