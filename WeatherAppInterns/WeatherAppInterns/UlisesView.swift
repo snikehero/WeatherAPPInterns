acee1b8dfbc67585b7008d86d4e405ae6e1aac66
UBurns <133381906+UBurns@users.noreply.github.com>
2023-05-16 09:26:28 -0700

    add UlisesView
    i added UlisesView

---
+++ WeatherAppInterns/WeatherAppInterns/UlisesView.swift
@@ -0,0 +1,20 @@
+//
+//  UlisesView.swift
+//  WeatherAppInterns
+//
+//  Created by Ulises Burns on 16/05/23.
+//
+
+import SwiftUI
+
+struct UlisesView: View {
+    var body: some View {
+        Text("Hola Weather")
+    }
+}
+
+struct UlisesView_Previews: PreviewProvider {
+    static var previews: some View {
+        UlisesView()
+    }
+}
